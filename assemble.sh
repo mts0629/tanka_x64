bf5183e02d14ff53fe4758c3f4da7b6e38c342f3
mts0629 <soratobi93@gmail.com>
2025-08-03 16:52:56 +0900

    Print "Waka"

---
+++ assemble.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -eu
+
+assemble() {
+    src=$1
+
+    fname=$(basename ${src/.S/})
+
+    mkdir -p ./bin
+    mkdir -p ./disassembly
+
+    gcc ${src} -o ./bin/${fname} && \
+    ./bin/${fname} && \
+    objdump -d ./bin/${fname} > ./disassembly/${fname}.d
+
+}
+
+assemble $1
